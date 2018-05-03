f81ac7ee669778647c712d8d5bd786dd2ec70dbd
chrono.legionaires@gmail.com <chrono.legionaires@gmail.com>
2018-05-03 23:53:39 +0700

    Fixed algorithm, Added Output to Excel

--- info-retrieval.py
+++ info-retrieval.py
@@ -1,4 +1,5 @@
 import math
+import xlsxwriter
 from textblob import TextBlob as tb
 from pathlib import Path
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
@@ -77,7 +78,7 @@ bloblist = blobers(tempCorpus)
 
 
 #queries
-query = 'rumah anda saya dipecat istri'
+query = 'rumah anda saya dipecat istri motor'
 query2 = 'istri anda di rumah'
 query3 = 'rumah saya ada di tangerang selatan'
 query4 = 'rumah aar ada istri'
@@ -86,7 +87,7 @@ query5 = 'istri aar di tilang'
 queries = [query]
 #queries = [query,query2,query3,query4,query5] #5 queries example
 
-
+sheet=[]
 #Stem and filters queries
 for i,q in enumerate(queries):
     temp = stopword.remove(q)
@@ -102,6 +103,7 @@ for q in queries:
     for x,s in enumerate(qsplit):
         counter = 0
         idf = sum(1 for blob in bloblist if s in blob.words)
+        print(idf)
         idf = math.log(len(bloblist)/1 + idf)
         tempIdf.append(idf)
         print('idf(', s,'): ', idf)
@@ -129,8 +131,7 @@ for q in queries:
         idf = sum(1 for blob in bloblist if s in blob.words)
         idf = math.log(len(bloblist)/1 + idf)
         for i, blob in enumerate(bloblistquery):
-            totalwords = sum(1 for word in blob.words)
-            tf = blob.words.count(s)/sum(1 for word in blob.words)
+            tf = blob.words.count(s)
             tfidf = tf*idf
             tempArrayQ.append(tfidf)
             
@@ -141,14 +142,17 @@ print(countrs)  #counter for corpus list
 
 #Cosine Similarities
 #deploy to find dotProducts
-def deploy(queryNo, docNo):
-    n = queryNo-1
+def deploy(docNo):
     m = docNo-1
+    total=0
     for i in range(len(tempArrayQ)):
         for x in range(countrs):
-            if n==i and m==x:
+            if m==x:
                 print(i+1,x+1,tempArrayQ[i],tempArray2[i][x],tempArrayQ[i]*tempArray2[i][x])
-                return tempArrayQ[i]*tempArray2[i][x]
+                total+=tempArrayQ[i]*tempArray2[i][x]
+                break
+
+    return total    
 
 #deploy2 to find related document frequencies
 def deploy2(docNo):
@@ -158,32 +162,67 @@ def deploy2(docNo):
         for i in range(countrs):
             if n==i:
                 print(x+1,i+1, tempArray2[x][i])
-                total+=tempArray2[x][i]*tempArray2[x][i]
+                total+=tempArray2[x][i]**2
                 break
     return total
 
 dotTotal=0
 qS = 0
 dS = 0
-
-#this is Document 1 tf idf
-for x in range(countrs):
-    dotTotal+=deploy(1,x+1) #deploy(query, whichDoc) in this case we use query 1 and doc 1
-
-    #P.S you can control the deploy value to search through docs with defined queries before
-    
-
+cos=[]
 #loop to find query frequencies
 total=0
 for x in tempArrayQ:
-    total+=x*x
+    total+=x**2
 
-qS = math.sqrt(total)
-dS = math.sqrt(deploy2(1))
-print(dS)
-dotS = dotTotal/(qS*dS)
+for a in range(countrs):
+    print("\nD"+str(a))
+    #this is Document 1 tf idf
+    dotTotal+=deploy(a+1) #deploy(query, whichDoc) in this case we use query 1 and doc 1
+    print("\ndot total "+str(dotTotal))
+    #P.S you can control the deploy value to search through docs with defined queries before
 
-#Cosine Similarity Result
-print(dotS)
+    temp=[]
+    
+    qS = math.sqrt(total)
+    dS = math.sqrt(deploy2(a+1))
+    print("\n"+str(total))
+    print("qs "+str(qS))
+    print("ds "+str(dS))
+    dotS = dotTotal/(qS*dS)
+
+    #Cosine Similarity Result
+    print("\n"+str(dotS))
+    temp.append("D"+str(a+1))
+    temp.append(dotS)
+    cos.append(temp)
+    temp=[]
+    dotTotal=0
+
+cos.sort(key=lambda x:x[1], reverse=True)
+print(cos)
+sheet.append("q1")
+sheet.append(cos)
+
+
+workbook = xlsxwriter.Workbook('cosine.xlsx')
+worksheet = workbook.add_worksheet()
+rowsheet = 0
+col = 0
+
+for x,i in enumerate(sheet):
+    if(x==1):
+        for d,c in enumerate(i):
+            if(d==0):
+                worksheet.write(rowsheet, d+1, str(c))
+                rowsheet+=1
+            else:
+                worksheet.write(rowsheet, d, str(c))
+                
+    else:
+        worksheet.write(rowsheet, col, str(i))
+
+    
+workbook.close()
         
 
