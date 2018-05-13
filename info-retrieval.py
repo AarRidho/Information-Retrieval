4e050c2c5524c64a85b91c4aadb2b59cfde541f1
chrono.legionaires@gmail.com <chrono.legionaires@gmail.com>
2018-05-13 17:06:17 +0700

    Fixed Algorithm

--- info-retrieval.py
+++ info-retrieval.py
@@ -1,5 +1,6 @@
 import math
 import xlsxwriter
+import operator
 from textblob import TextBlob as tb
 from pathlib import Path
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
@@ -68,166 +69,125 @@ def idf(word, bloblist):
 def tfidf(word, blob, bloblist):
     return tf(word, blob) * idf(word, bloblist)
 
+def vector(arr):
+    temp = 0
+    for i in arr:
+        temp+=(i**2)
+
+    return temp
+
+def safe_abs(x):
+    if(x==0):
+        return 0
+    else:
+        return abs(x)
+
+def safe_div(x,y):
+    if y==0:
+        return 0
+    return x/y
 
 #Stemmed and filtered corpus right here
 print(tempCorpus)
 
-
-#using textblob for additional filtering
-bloblist = blobers(tempCorpus)
-
-
 #queries
-query = 'rumah anda saya dipecat rumah istri motor andalan'
+query = 'kontur tanah dan udara lebih baik'
 query2 = 'istri anda di rumah'
 query3 = 'rumah saya ada di tangerang selatan'
 query4 = 'rumah aar ada istri'
 query5 = 'istri aar di tilang'
 
 queries = [query]
-#queries = [query,query2,query3,query4,query5] #5 queries example
+
+#using textblob for additional filtering
+bloblist = blobers(tempCorpus)
+bloblistquery = blobers(queries)
 
 sheet=[]
+
 #Stem and filters queries
 for i,q in enumerate(queries):
     temp = stopword.remove(q)
     queries[i] = stemmer.stem(temp)
 
-tempArray = []
-tempArray2 = []
-tempIdf = []
-tempIdf2 = []
-for q in queries:
-    print('\nquery:', q)
-    qsplit = q.split()
-    print("qsplit: "+str(qsplit))
-    for x,s in enumerate(qsplit):
-        counter = 0
-        idf = sum(1 for blob in bloblist if s in blob.words)
-        idf = math.log(len(bloblist)/(1 + idf))
-        tempIdf.append(idf)
-        IDF=idf
-        print('idf(', s,'): ', idf)
-        print(s,'\ntf:')
-        for i, blob in enumerate(bloblist):
-            print('total words', sum(1 for word in blob.words))
-            print('D',i+1,': ', blob.words.count(s),', tf.idf: ', blob.words.count(s)*idf)
-            tempArray.append(blob.words.count(s)*idf)
-            counter += blob.words.count(s)
-        tempArray2.append(tempArray)
-        tempArray = []
-        print('tftotal(', s,'): ', counter)
-        print('\n')
-    print('\n')
-    tempIdf2.append(tempIdf)
+tempTFIDF = []
+tempTFIDFArray = []     #1
+tempTFIDFquery = []
+tempTFIDFqueryA = []    #2
+tempcos = []
 
-print("tempArray2")
-print(tempArray2,'\n')
+#TFIDF Documents
+for i, blobs in enumerate(bloblistquery):
+    for x,blobd in enumerate(bloblist):
+        scores = {s: tfidf(s, blobd, bloblist) for s in blobs.words}
+        sorted_words = scores.items()
+        print("\n\tD"+str(x+1)+":")
+        tempArr = []
+        for word, score in sorted_words:
+            print("\tWord: {}, TF-IDF: {}".format(word, score))
+            tempArr.append(score)
 
-tempArrayQ = []         #query tf.idf
-tempArrayQ2 = []
-bloblistquery = blobers(queries)
-for q in queries:
-    qsplit = q.split()
-    for x,s in enumerate(qsplit):
-        print("\ntf: "+str(idf))
-        for i, blob in enumerate(bloblistquery):
-            tf = blob.words.count(s)
-            print(tf)
-            tfidf = tf*IDF
-            tempArrayQ.append(tfidf)
+        tempTFIDFArray.append(tempArr)
+        tempTFIDF.append(scores)
+
+print(tempTFIDF)
+print(tempTFIDFArray)
+
+#TFIDF Query
+for i, blobs in enumerate(bloblistquery):
+    for x,blobd in enumerate(bloblist):
+        scores = {s: tfidf(s, blobs, bloblist) for s in blobs.words}
+        sorted_words = scores.items()
+        print("Q"+str(x+1)+":")
+        tempArr = []
+        for word, score in sorted_words:
+            print("\tWord: {}, TF-IDF: {}".format(word, score))
+            tempTFIDFqueryA.append(score)
+
+        tempTFIDFquery.append(scores)
+        break
             
-print(tempArrayQ)
-print(bloblistquery)
+print(tempTFIDFquery)
+print(tempTFIDFqueryA)
 
-print(countrs)  #counter for corpus list
+#cosine similarity
+for y,scd in enumerate(tempTFIDFArray):
+    temp=0
+    for z,scz in enumerate(scd):
+        temp+=(tempTFIDFqueryA[z]*scz)
 
-
-#Cosine Similarities
-#deploy to find dotProducts
-def deploy(docNo):
-    m = docNo-1
-    total=0
-    for i in range(len(tempArrayQ)):
-        for x in range(countrs):
-            if m==x:
-                print(i+1,x+1,tempArrayQ[i],tempArray2[i][x],tempArrayQ[i]*tempArray2[i][x])
-                total+=tempArrayQ[i]*tempArray2[i][x]
-                break
-
-    return total    
-
-#deploy2 to find related document frequencies
-def deploy2(docNo):
-    n = docNo-1
-    total = 0
-    for x in range(len(tempArray2)):
-        for i in range(countrs):
-            if n==i:
-                print(x+1,i+1, tempArray2[x][i])
-                total+=(tempArray2[x][i])**2
-                break
-    return total
-
-dotTotal=0
-qS = 0
-dS = 0
-cos=[]
-#loop to find query frequencies
-total=0
-for x in tempArrayQ:
-    total+=x**2
-
-print("finding frequencies")
-
-for a in range(countrs):
-    print("\nD"+str(a))
-    #this is Document 1 tf idf
-    dotTotal+=deploy(a+1) #deploy(whichDoc) in this case we use query 1 and doc 1
-    print("\ndot total "+str(dotTotal))
-    #P.S you can control the deploy value to search through docs with defined queries before
-
-temp=[]
+    vectorQ = safe_abs(math.sqrt(vector(tempTFIDFqueryA)))
+    vectorD = safe_abs(math.sqrt(vector(scd)))
     
-qS = math.sqrt(total)
-dS = math.sqrt(deploy2(a+1))
-print("\n"+str(total))
-print("qs "+str(qS))
-print("ds "+str(dS))
-dotS = dotTotal/(qS*dS)
+    print("\nDotProduct(Q, D"+str(y)+"):"+str(temp))
+    print("\nvectorQ: "+str(vectorQ))
+    print("\nvectorD: "+str(vectorD))
 
-#Cosine Similarity Result
-print("\n"+str(dotS))
-temp.append("D"+str(a+1))
-temp.append(dotS)
-cos.append(temp)
-temp=[]
-dotTotal=0
+    cos=safe_div(temp,(vectorQ*vectorD))
+    print("Cos(Q, D1):"+str(cos))
+    tempcos.append(cos)
+    
+print(tempcos)  
 
-cos.sort(key=lambda x:x[1], reverse=True)
-print(cos)
-sheet.append("q1")
-sheet.append(cos)
+tempLastDict = {}
 
+for i,x in enumerate(tempcos):
+    tempLastDict["D"+str(i+1)] = x
 
-workbook = xlsxwriter.Workbook('cosine.xlsx')
+print(tempLastDict)
+
+_excel = sorted(tempLastDict.items(), key=operator.itemgetter(1),reverse=True)
+print(_excel)
+
+workbook = xlsxwriter.Workbook('cosineq1.xlsx')
 worksheet = workbook.add_worksheet()
-rowsheet = 0
-col = 0
 
-for x,i in enumerate(sheet):
-    if(x==1):
-        for d,c in enumerate(i):
-            if(d==0):
-                worksheet.write(rowsheet, d+1, str(c))
-                rowsheet+=1
-            else:
-                worksheet.write(rowsheet, d, str(c))
-                
-    else:
-        worksheet.write(rowsheet, col, str(i))
+row=0
+col=0
 
-    
+for doc, cos in _excel:
+    worksheet.write(row, col, doc)
+    worksheet.write(row, col+1, cos)
+    row +=1
+
 workbook.close()
-        
-
